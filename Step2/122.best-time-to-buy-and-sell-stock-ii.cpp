ddc34e69f4cadc52dd86e486ecf2529cc456d1af
Mouni Nischal <d.chiranjeevi007@gmail.com>
2021-06-30 23:17:41 +0530

    lol easy | arrays

---
+++ Step2/122.best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=122 lang=cpp
+ *
+ * [122] Best Time to Buy and Sell Stock II
+ */
+#include <bits/stdc++.h>
+using namespace std;
+// @lc code=start
+class Solution
+{
+public:
+  int maxProfit(vector<int> &prices)
+  {
+    int result = 0;
+    if (prices.size() <= 1)
+      return 0;
+    for (int i = 1; i < prices.size(); i++)
+    {
+      if (prices[i] > prices[i - 1])
+      {
+        result += (prices[i] - prices[i - 1]);
+      }
+    }
+    return result;
+  }
+};
+// @lc code=end
