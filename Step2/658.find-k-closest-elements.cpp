2814b49bb8ee875d3628a26a4d53d4233b6a3af3
Mouni Nischal <d.chiranjeevi007@gmail.com>
2021-07-03 21:06:30 +0530

    optimized solution | BST, ARRAYS

--- Step2/658.find-k-closest-elements.cpp
+++ Step2/658.find-k-closest-elements.cpp
@@ -8,18 +8,33 @@ using namespace std;
 // @lc code=start
 class Solution {
 public:
-  vector<int> findClosestElements(vector<int> &arr, int k, int x) {
-    int left = 0, right = arr.size() - 1;
-
-    while (right - left + 1 > k) {
-
-      if (abs(arr[right] - x) < abs(x - arr[left]))
-        left++;
-      else
-        right--;
+  vector<int> findClosestElements(vector<int> &A, int k, int x) {
+    int left = 0, right = A.size() - k;
+    while (left < right) {
+      int mid = (left + right) / 2;
+      if (x - A[mid] > A[mid + k] - x) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
     }
-
-    return vector<int>(arr.begin() + left, arr.begin() + right + 1);
+    return vector<int>(A.begin() + left, A.begin() + left + k);
   }
 };
+// class Solution {
+// public:
+//   vector<int> findClosestElements(vector<int> &arr, int k, int x) {
+//     int left = 0, right = arr.size() - 1;
+
+//     while (right - left + 1 > k) {
+
+//       if (arr[right] - x < x - arr[left])
+//         left++;
+//       else
+//         right--;
+//     }
+
+//     return vector<int>(arr.begin() + left, arr.begin() + right + 1);
+//   }
+// };
 // @lc code=end
