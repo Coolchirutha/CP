ae88ece29d1223d86c55685048b4801b2c445821
Mouni Nischal <d.chiranjeevi007@gmail.com>
2021-07-23 18:14:55 +0530

    Nice conceptual question | Linked Lists

--- Step2/160.intersection-of-two-linked-lists.cpp
+++ Step2/160.intersection-of-two-linked-lists.cpp
@@ -3,21 +3,65 @@
  *
  * [160] Intersection of Two Linked Lists
  */
+#include <bits/stdc++.h>
+using namespace std;
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
 
 // @lc code=start
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        
+  ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    ListNode *p1 = headA;
+    ListNode *p2 = headB;
+
+    if (p1 == NULL || p2 == NULL) return NULL;
+
+    while (p1 != NULL && p2 != NULL && p1 != p2) {
+        p1 = p1->next;
+        p2 = p2->next;
+
+        //
+        // Any time they collide or reach end together without colliding
+        // then return any one of the pointers.
+        //
+        if (p1 == p2) return p1;
+
+        //
+        // If one of them reaches the end earlier then reuse it
+        // by moving it to the beginning of other list.
+        // Once both of them go through reassigning,
+        // they will be equidistant from the collision point.
+        //
+        if (p1 == NULL) p1 = headB;
+        if (p2 == NULL) p2 = headA;
     }
+
+    return p1;
+  }
 };
 // @lc code=end
 
+// My solution(slower and more space consuming).
+class Solution {
+public:
+  ListNode *getIntersectionNode(ListNode *head1, ListNode *head2) {
+    unordered_map<ListNode *, int> m;
+    ListNode *itr = head1;
+    while(itr){
+      m.insert({itr, 1});
+      itr = itr->next;
+    }
+    itr = head2;
+    while(itr){
+      if(m.count(itr)){
+        return itr;
+      }
+      itr = itr->next;
+    }
+    return NULL;
+  }
+};
