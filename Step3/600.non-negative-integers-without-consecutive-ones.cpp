c3fb70249b254ab617dc1790118c0b8bc71df1b3
Mouni Nischal <d.chiranjeevi007@gmail.com>
2021-07-26 12:03:35 +0530

    Nice question | DIGIT DP

---
+++ Step3/600.non-negative-integers-without-consecutive-ones.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=600 lang=cpp
+ *
+ * [600] Non-negative Integers without Consecutive Ones
+ */
+
+// @lc code=start
+class Solution {
+public:
+  int findIntegers(int num) {
+    int f[32];
+    f[0] = 1;
+    f[1] = 2;
+    for (int i = 2; i < 32; ++i)
+      f[i] = f[i - 1] + f[i - 2];
+    int ans = 0, k = 30, pre_bit = 0;
+    while (k >= 0) {
+      if (num & (1 << k)) {
+        ans += f[k];
+        if (pre_bit)
+          return ans;
+        pre_bit = 1;
+      } else
+        pre_bit = 0;
+      --k;
+    }
+    return ans + 1;
+  }
+};
+// @lc code=end
