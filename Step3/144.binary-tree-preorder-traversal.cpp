df6b3290d4e6bce528770769f6540065702590f6
Mouni Nischal <d.chiranjeevi007@gmail.com>
2021-08-20 06:58:47 +0530

    Morris traversal | Tree traversal

--- Step3/144.binary-tree-preorder-traversal.cpp
+++ Step3/144.binary-tree-preorder-traversal.cpp
@@ -24,6 +24,45 @@ public:
   }
   vector<int> preorderTraversal(TreeNode *root) {
     vector<int> result;
+    while (root) {
+      if (root->left) {
+        // Searching for the right-most leaf in the left sub-tree of root.
+        TreeNode *itr = root->left;
+        while (itr->right && itr->right != root) {
+          itr = itr->right;
+        }
+        if (!itr->right) {
+          // If right doesn't exist we set it to root and print val and go to
+          // the left subtree to traverse it first.
+          itr->right = root;
+          result.push_back(root->val);
+          root = root->left;
+        } else {
+          // Comes here when itr->right == root. This means lef traversal is
+          // done and now right traversal has to take place.
+          itr->right = NULL;
+          root = root->right;
+        }
+      } else {
+        // When root doesn't have a left subtree, we print cur and go right.
+        result.push_back(root->val);
+        root = root->right;
+      }
+    }
+    return result;
+  }
+};
+// @lc code=end
+
+// Using stack
+class Solution {
+public:
+  Solution() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+  }
+  vector<int> preorderTraversal(TreeNode *root) {
+    vector<int> result;
     stack<TreeNode *> s;
     while (root || !s.empty()) {
       if (root) {
@@ -40,4 +79,3 @@ public:
     return result;
   }
 };
-// @lc code=end
