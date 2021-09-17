4cdc4716c0abfa96147201030e378d0e02b8ec5e
Mouni Nischal <d.chiranjeevi007@gmail.com>
2021-09-17 19:30:19 +0530

    NICE QUESTION | Binary Tree

--- Step3/112.path-sum.cpp
+++ Step3/112.path-sum.cpp
@@ -19,17 +19,43 @@ struct TreeNode {
 class Solution {
 public:
   bool hasPathSum(TreeNode *root, int target) {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    if (!root) {
+    if (!root)
       return false;
-    }
-
-    if (root->val == target && root->left == NULL && root->right == NULL) {
+    if (root->left == NULL && root->right == NULL && target - root->val == 0) {
       return true;
     }
-
-    return hasPathSum(root->left, target - root->val) || hasPathSum(root->right, target - root->val);
+    return (hasPathSum(root->left, target - root->val) ||
+            hasPathSum(root->right, target - root->val));
   }
 };
 // @lc code=end
+class Solution {
+public:
+  bool hasPathSum(TreeNode *root, int target) {
+    if (!root)
+      return false;
+
+    stack<TreeNode *> nodes;
+    stack<int> sums;
+    nodes.push(root);
+    sums.push(target - root->val);
+    while (!nodes.empty()) {
+      TreeNode *cur = nodes.top();
+      nodes.pop();
+      int curSum = sums.top();
+      sums.pop();
+      if (cur->left == NULL && cur->right == NULL && curSum == 0) {
+        return true;
+      }
+      if (cur->left) {
+        nodes.push(cur->left);
+        sums.push(curSum - cur->left->val);
+      }
+      if (cur->right) {
+        nodes.push(cur->right);
+        sums.push(curSum - cur->right->val);
+      }
+    }
+    return false;
+  }
+};
