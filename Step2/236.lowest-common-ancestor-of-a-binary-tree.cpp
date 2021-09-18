c0909d9d4b93276659dfbcfe12a2ff1f1b34ab91
Mouni Nischal <d.chiranjeevi007@gmail.com>
2021-09-18 18:50:22 +0530

    NICE QUESTION | Binary Tree

--- Step2/236.lowest-common-ancestor-of-a-binary-tree.cpp
+++ Step2/236.lowest-common-ancestor-of-a-binary-tree.cpp
@@ -3,17 +3,16 @@
  *
  * [236] Lowest Common Ancestor of a Binary Tree
  */
-
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
 // @lc code=start
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution
 {
 public:
@@ -23,12 +22,10 @@ public:
       return root;
     TreeNode *left = lowestCommonAncestor(root->left, p, q);
     TreeNode *right = lowestCommonAncestor(root->right, p, q);
-    if (left == NULL)
-      return right;
-    else if (right == NULL)
-      return left;
-    else
+    if (left && right)
       return root;
+    else
+      return left ? left : right;
   }
 };
 // @lc code=end
